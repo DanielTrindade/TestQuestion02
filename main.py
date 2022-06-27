4d8f8c1408590bbd39c00234f60200f62ea5282b
DANIEL DE <danieloliveiratrindade@gmail.com>
2022-06-27 19:05:43 +0000

    Realização da função que calcula o menor valor.

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import json
+
 #função para calcular a media
 def calcMedia(dados):
   dias = 0;
   acumulador = 0.0
   media = 0.0
   for dado in dados:
-    if(dado['valor'] != 0.0):
-      dias++;
+    if dado['valor'] != 0.0:
+      print(dado['valor'])
+      dias += 1;
       acumulador +=dado['valor']
   #calculo da media
+  print(acumulador,dias)
   media = acumulador / dias
   return media
 
-with open("dados.json", encoding='utf-8') as meu_json:
-    dados = json.load(meu_json)
+#função para encontrar o menor valor no mês
+def calcMenor(dados):
+  #retorna o primeiro valor que não é 0.0(Zero)
+  menor = dados[0]
+  pos = 0
+  while(menor['valor'] == 0.0):
+    menor = dados[pos]
+    pos += 1
+  #verifica o menor de fato ignorando os valores
+  for dado in dados[(pos + 1):]:
+    if dado['valor'] != 0.0:
+      if(menor['valor'] > dado['valor']):
+        menor = dado
 
-for dado in dados:
-  print('No dia',dado['dia'],'o valor foi de',dado['valor'])
+  return menor
+
+
+if(__name__ == '__main__'):
+  with open("dados.json", encoding='utf-8') as meu_json:
+      dados = json.load(meu_json)
+  media = calcMedia(dados)
+  menor = calcMenor(dados)
+  print(menor)
